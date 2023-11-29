0bd6b49e55fd8c36234b96fc2f06cc327e3e9527
gramizor <granin17@yandex.ru>
2023-11-30 01:13:52 +0300

    checkPhoneNumber to employees

--- src/pages/Employee.tsx
+++ src/pages/Employee.tsx
@@ -32,6 +32,17 @@ const extractKeys = (dataTable: IInfoEmployee[] | null) => {
   return [];
 };
 
+const checkPhoneNumber = (phoneNumber: string): string => {
+  const cleaned = phoneNumber.replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})$/);
+  if (match) {
+    return `+${match[1]} (${match[2]}) ${match[3]}-${match[4]}-${match[5]}`;
+  } else {
+    return ("Неверный формат номера")
+  }
+  return phoneNumber;
+};
+
 const transformData = (data: IEmployee[] | null) => {
   return (
     data?.map((item) => {
@@ -49,7 +60,7 @@ const transformData = (data: IEmployee[] | null) => {
         jobPosition,
         salary,
         mail: email,
-        phoneNumber,
+        phoneNumber: checkPhoneNumber(phoneNumber),
       };
     }) || []
   );
